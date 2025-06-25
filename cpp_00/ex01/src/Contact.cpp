85ef7e3a17df68967e97a510259978411641c646
simy404 <asimetra404@gmail.com>
2025-06-25 14:15:45 +0300

    refactor: replace setter methods with getter methods in Contact class

--- cpp_00/ex01/src/Contact.cpp
+++ cpp_00/ex01/src/Contact.cpp
@@ -1,28 +1,26 @@
 #include "Contact.hpp"
 
-void	Contact::set_first_name(std::string first_name)
+std::string 	Contact::get_first_name() const
 {
-	_first_name = first_name;
+	return _first_name;
+}
+std::string 	Contact::get_last_name() const
+{
+	return _last_name;
 }
 
-void	Contact::set_last_name(std::string last_name)
+std::string 	Contact::get_nickname() const
 {
-	_last_name = last_name;
+	return _nickname;
+}
+std::string 	Contact::get_phone_number() const
+{
+	return _phone_number;
 }
 
-void	Contact::set_nickname(std::string nickname)
+std::string 	Contact::get_darkest_secret() const
 {
-	_nickname = nickname;
-}
-
-void	Contact::set_phone_number(std::string phone_number)
-{
-	_phone_number = phone_number;
-}
-
-void	Contact::set_darkest_secret(std::string darkest_secret)
-{
-	_darkest_secret = darkest_secret;
+	return _darkest_secret;
 }
 
 Contact::Contact()
