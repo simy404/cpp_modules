47a0be5a797cd0a6dd455790d42328d7ac98fbeb
simy404 <asimetra404@gmail.com>
2025-08-25 19:33:17 +0300

    fix:some kind of bugs

--- cpp_05/ex03/src/Intern.cpp
+++ cpp_05/ex03/src/Intern.cpp
@@ -7,7 +7,7 @@ Intern::Intern() {};
 
 Intern::Intern(const Intern& intern) {}
 
-Intern&	Intern::operator=(const Intern& intern) {};
+Intern&	Intern::operator=(const Intern& intern) { return *this};
 
 AForm *Intern::makeRobotomyRequestForm(const std::string& target) {return new RobotomyRequestForm(target);}
 
@@ -19,9 +19,20 @@ AForm	*Intern::makeForm(std::string formName, std::string target)
 {
 	typedef AForm* (Intern::*FormMaker)(const std::string& _target);
 
-	FormMaker	maker[3] = {makeRobotomyRequestForm, makePresidentialPardonForm, makeShrubberyCreationForm};
-	std::string	FormNames[3] = {"roboto myRequest", "presidential pardon", "shrubbery creation"};
+	FormMaker	maker[3] = {
+		&Intern::makeRobotomyRequestForm,
+		&Intern::makePresidentialPardonForm,
+		&Intern::makeShrubberyCreationForm
+	};
+
+	std::string	FormNames[3] = {
+		"roboto myRequest",
+		"presidential pardon",
+		"shrubbery creation"
+	};
+
 	AForm*		form = NULL;
+
 	for (size_t i = 0; i < 3; i++) {
 		if (target == FormNames[i]) {
 			form = (this->*maker[i])(target);
@@ -29,6 +40,6 @@ AForm	*Intern::makeForm(std::string formName, std::string target)
 		}
 	}
 
-
+	return form;
 }
 Intern::~Intern() {};
